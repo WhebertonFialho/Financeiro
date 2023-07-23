616c7cf7d2338e24d2889db86c1d855b4efbb2ac
Wheberton Fialho <whebertonfialho@gmail.com>
2023-07-23 08:43:21 -0400

    config

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -1,15 +1,33 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { useTheme } from 'styled-components/native';
 
 import { Home } from '@screens/home';
 import { Configuracao } from '@screens/configuracao';
 
+import type { StackNavigationOptions } from '@react-navigation/stack';
 const { Navigator, Screen } = createDrawerNavigator();
 
 export function AppRoutes(){
+  const { COLORS } = useTheme();
+
+  const slyleMenu : StackNavigationOptions = {
+      //headerShown: false,
+      drawerActiveBackgroundColor: {
+        color: '#000'
+      },
+      drawerLabelStyle: {
+        color: COLORS.WHITE
+      },
+      drawerStyle: {
+        backgroundColor: COLORS.GRAY_500,
+        width: 240
+      } 
+  }
+
   return(
-    <Navigator>
+    <Navigator screenOptions={ slyleMenu } >
       <Screen name="Home" component={ Home } />
-      <Screen name="Configuracao" component={Configuracao} />
+      <Screen name="Configuração" component={ Configuracao } />
     </Navigator>
   );
 }
